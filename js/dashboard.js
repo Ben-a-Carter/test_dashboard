adef7523c935a2d54235b5014932044e6e093355
Ben-a-Carter <82947387+Ben-a-Carter@users.noreply.github.com>
2026-08-13 13:10:37 -0400

    Remove filterTitle update in buildUI function

--- js/dashboard.js
+++ js/dashboard.js
@@ -89,7 +89,6 @@ function switchTab(key){
 }
 function buildUI(){
   const c=configs[activeTab];
-  document.getElementById('filterTitle').textContent=c.title;
   document.getElementById('dashboardKicker').textContent=c.title;
   document.getElementById('dashboardTitle').textContent=c.title;
   document.getElementById('dashboardDescription').textContent=c.description;
